88bcff354a0fba8307c44f86da66a744553ce2db
Miguel Chicaiza <machicaiza11@espe.edu.ec>
2022-05-17 00:43:04 -0500

    First commit

---
+++ OperacionesAritmeticos.py
@@ -0,0 +1,31 @@
+#Ingreso de dos números mediante teclado
+#Se ingresa el valor a las variables num1 y num2 mediante teclado 
+num1=int(input("Ingrese el primer numero: "))
+num2=int(input("Ingrese el segundo numero: "))
+#Operaciones Matemáticas
+#Operación Suma
+suma=num1+num2
+#Operación Resta
+resta=num1-num2
+#Operación Multiplicación
+mult=num1*num2
+#Operación División
+div=num1/num2
+#Operación Exponente 
+expo=num1**num2
+#Operación División Entera
+ent=num1//num2
+#Operación Modulo
+mod=num1%num2
+#Respuesta por mensaje de cada Operación Matematico
+print("La Suma = ",suma)
+print("La Resta = ",resta)
+print("La Multiplicacion = ",mult)
+print("La Divicion = ",div)
+print("El Exponente = ",expo)
+print("El Entero = ",ent)
+print("El Modulo = ",mod)
+
+
+
+
